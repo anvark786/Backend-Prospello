50e6e5d71135da5340a3d0574715ce1cbc2f9203
Your Name <mohammedanvark.talrop@gmail.com>
2022-01-11 10:33:25 +0530

    new

--- prospello/urls.py
+++ prospello/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('web.urls',namespace="web")),
+    path('prospello-admin/', admin.site.urls),
+    path('', include('web.urls', namespace="web")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Prospello Administration'
+admin.site.site_title = 'Sit administration | Prospello site admin'
